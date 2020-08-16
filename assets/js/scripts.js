afb4def9977368f92fdff19a38d3b0b793edbe0d
theseangoodwin <seanmgoodwin1@gmail.com>
2020-08-16 12:45:38 -0400

    updating dir and adding interactjs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,1 @@
-
-// Get the element
-var elem = document.querySelector('#entry_12');
-// Create a copy of it
-var clone = elem.cloneNode(true);
-// Update the ID and add a class
-clone.id = 'entry_12b';
-clone.classList.add('rotate_entry_b');
-// Inject it into the DOM
-elem.after(clone);
+import interact from 'interactjs'
