7584e90bdb664f3f3e1072fff27b8e9e11f63d3d
theseangoodwin <seanmgoodwin1@gmail.com>
2021-01-06 22:20:23 -0500

    updating the correct margin css props

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -6,16 +6,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 });
 
-/* Set the left margin of the page content to 0 */
-function openNav() {
-  document.getElementById("mySidebar").style.marginLeft = "0px";
-  console.log('Open, Console!');
-//   document.getElementById("main").style.marginLeft = "370px";
-}
-
 /* Set the left margin of the page content to -370 */
 function closeNav() {
   document.getElementById("mySidebar").style.marginLeft = "-370px";
   console.log('Closed, Console!');
-//   document.getElementById("main").style.marginLeft = "0";
 }
+
+/* Set the left margin of the page content to 0 */
+function openNav() {
+    document.getElementById("mySidebar").style.marginLeft = "0px";
+    console.log('Open, Console!');
+  }
